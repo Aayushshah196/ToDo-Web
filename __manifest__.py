21e8c221badcbb05040496c09bc7f5c2faa1af7e
Aayushshah196 <aayushshahkanu@gmail.com>
2022-12-12 16:45:14 +0545

    Initialize Project Structure

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Todo",
+    "version": "1.0",
+    "author": "Aayush Shah Kanu",
+    "category": "Personal",
+    "description": """
+        A Todo App made using client action
+    """,
+    "depends": [
+        "base",
+        "web",
+        "website",
+    ],
+    "qweb": [
+    ],
+    "data": [
+    ],
+    "sequence": "-1",
+    "installable": True,
+    "application": True,
+}
